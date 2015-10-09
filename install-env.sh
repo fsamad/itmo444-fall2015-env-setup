ac3973bfb42ccab70d166d26e6261d36912941e8
fsamad <fabdelsa@hawk.iit.edu>
2015-10-09 15:30:01 -0500

    // Setup enviroment code

---
+++ install-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get install -y apache2 git
+
+git clone https://github.com/fsamad/itmo444-fall2015-app-setup.git
+
+mv ./itmo444-fall2015-app-setup/images /var/www/html/images
+mv ./itmo444-fall2015-app-setup/index.html /var/www/html
+mv ./itmo444-fall2015-app-setup/page2.html /var/www/html
+
+echo "Farah Abdul Samad" /tmp/hello.txt    
